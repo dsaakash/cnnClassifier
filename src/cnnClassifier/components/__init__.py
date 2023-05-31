90ada7b0bd5a370331e4c0f09419900a3c19931b
Aakash <savantaakash322@gmail.com>
2023-06-01 02:25:04 +0530

    training added

--- src/cnnClassifier/components/__init__.py
+++ src/cnnClassifier/components/__init__.py
@@ -1,6 +1,7 @@
 from cnnClassifier.components.data_ingestion import DataIngestion
 from cnnClassifier.components.prepare_base_model import PrepareBaseModel
 from cnnClassifier.components.prepare_callback import PrepareCallback
+from cnnClassifier.components.training import  Training
 
 
 
